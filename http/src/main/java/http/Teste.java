0119e6ad168cdf688d52d2ecf3da88eee6316b58
unknown <mjonatas@ymail.com>
2022-09-03 22:49:22 -0300

    Criado projeto http

---
+++ http/src/main/java/http/Teste.java
@@ -0,0 +1,18 @@
+package http;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> teste= new ArrayList<>();
+		
+		
+		System.out.println("teste");
+
+	}
+
+}
